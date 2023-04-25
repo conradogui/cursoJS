9535a4a95ebe9f85531d54f5840b4fc1f4b00231
conradogui <gui14fbce@gmail.com>
2023-04-25 18:01:53 -0300

    finish study

--- examples/estudos 30/index.js
+++ examples/estudos 30/index.js
@@ -1,13 +1,6 @@
 //Filter -> sempre retorna um array com a mesma quantidade de elementos ou menos
 //retornar os numeros maiores que 10
-const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
-/*
-for(let numero of numeros){
-    if(numero > 10) {
-        console.log(numero) --> [ 50, 80, 11, 15, 22, 27 ]
-    }
-}
-*/                                               
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]                                               
 const numerosFiltrados = numeros.filter(valor => valor > 10) //se acostumando com as arrow functions
 console.log(numerosFiltrados)// [ 50, 80, 11, 15, 22, 27 ]
 
@@ -22,9 +15,26 @@ const pessoas = [
 //retornar as pessoas que tem o nome com 5 letras ou mais
 const pessoasComNomeGrande = pessoas.filter(valor => valor.nome.length >= 5)
 console.log(pessoasComNomeGrande)
+/*
+[
+  { nome: 'Maria', idade: 23 },
+  { nome: 'Eduarda', idade: 55 },
+  { nome: 'Leticia', idade: 19 },
+  { nome: 'Rosana', idade: 32 },
+  { nome: 'Wallace', idade: 47 }
+]
+*/ 
 //retorna as pessoas que tem mais de 50 anos
 const pessoasVelhas = pessoas.filter(valor => valor.idade > 50)
-console.log(pessoasVelhas)
+console.log(pessoasVelhas)//[ { nome: 'Luiz', idade: 62 }, { nome: 'Eduarda', idade: 55 } ]
 //pessoas que terminam com a letra a
-const pessoasComa = pessoas.filter(valor => valor.nome.toLowerCase().endsWith('a'))
+const pessoasComa = pessoas.filter(valor => valor.nome.toLowerCase().endsWith('a'))//endsWith é que termina com tal coisa
 console.log(pessoasComa)
+/*
+[
+  { nome: 'Maria', idade: 23 },
+  { nome: 'Eduarda', idade: 55 },
+  { nome: 'Leticia', idade: 19 },
+  { nome: 'Rosana', idade: 32 }
+]
+*/
