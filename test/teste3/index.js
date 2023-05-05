779d02e486346173159b9837b86ebd8e44f60b60
conradogui <gui14fbce@gmail.com>
2023-05-05 13:01:40 -0300

    morning study

---
+++ test/teste3/index.js
@@ -0,0 +1,9 @@
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27] 
+const numersFiltro = numeros.filter(val => val > 11)
+console.log(numersFiltro)
+
+const numeros2 = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27] 
+const numis = numeros2.map(val => val * 2)
+console.log(numis)
+
+const numeros3 = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27] 
