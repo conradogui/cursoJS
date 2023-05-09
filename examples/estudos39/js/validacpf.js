e90ea4f2b8ef688590fb2d78cd35b30c5ca1e24a
conradogui <gui14fbce@gmail.com>
2023-05-09 10:39:27 -0300

    Criando formulario

--- examples/estudos39/js/validacpf.js
+++ examples/estudos39/js/validacpf.js
@@ -34,11 +34,11 @@ class ValidaCPF {
         return digito <= 9 ? String(digito) : '0'
     }
 }
-let validacpf = new ValidaCPF('070.987.720-03')
-// validacpf = new ValidaCPF('999.999.999-99')
+// let validacpf = new ValidaCPF('070.987.720-03')
+// // validacpf = new ValidaCPF('999.999.999-99')
 
-if(validacpf.valida()) {
-    console.log( 'CPF válido')
-} else {
-    console.log( 'CPF inválido')
-}
+// if(validacpf.valida()) {
+//     console.log( 'CPF válido')
+// } else {
+//     console.log( 'CPF inválido')
+// }
