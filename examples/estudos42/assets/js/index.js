51cd3048dc863e976b90adeb136d38a802de112a
conradogui <gui14fbce@gmail.com>
2023-05-23 11:15:35 -0300

    review

--- examples/estudos42/assets/js/index.js
+++ examples/estudos42/assets/js/index.js
@@ -1,14 +1,14 @@
 const request = obj => {
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest() 
-        xhr.open(obj.method, obj.url, true) 
-        xhr.send()
+        xhr.open(obj.method, obj.url, true) //Estamos configurando a solicitação HTTP com base nas informações fornecidas no objeto obj. obj.method contém o método HTTP (por exemplo, GET, POST) e obj.url contém a URL de destino para a solicitação.
+        xhr.send() //Estamos enviando a solicitação HTTP para o servidor
     
         xhr.addEventListener('load', () => {
-            if(xhr.status >= 200 && xhr.status < 300) { 
-                resolve(xhr.responseText) 
+            if(xhr.status >= 200 && xhr.status < 300) { //Estamos verificando se o código de status da resposta HTTP está na faixa de 200 a 299, o que indica uma resposta bem-sucedida
+                resolve(xhr.responseText) // xhr.responseText contém o corpo da resposta HTTP.
             } else {
-                reject(xhr.statusText)
+                reject(xhr.statusText)//xhr.statusText contém uma descrição do status da resposta HTTP.
             }
         })
     })
--- test/teste4/index.js
+++ test/teste4/index.js
@@ -1,33 +1,17 @@
-function rand(min=1, max=3) {
-    max *= 1000
-    min *= 1000
-    return Math.floor(Math.random() * (max - min) + min)
-}
-
-function espera(msg, tmp) {
+const request = obj => {
     return new Promise((resolve, reject) => {
-        if(typeof msg !== 'string') reject('Erro aqui')
-        setTimeout(() => {
-            resolve(msg)
-        }, tmp)
+        const xhr = new XMLHttpRequest
+        xhr.open(obj.method, obj.url, true)
+        xhr.send()
+
+        xhr.addEventListener('load', () => {
+            if(xhr.status >= 200 && xhr.status <= 300) {
+                resolve(xhr.responseText)
+            } else {
+                reject(xhr.statusText)
+            }
+        })
     })
 }
 
-async function faz() {
-    try {
-        const faz1 = await espera('Primeira mensagem', rand())
-        console.log(faz1)
 
-        const faz2 = await espera('Segunda mensagem da promessa', rand())
-        console.log(faz2)
-
-        const faz3 = await espera('Terceira mensagem', rand())
-        console.log(faz3)
-
-        const faz4 = await espera('Mensagem final', rand())
-        console.log(faz4)
-    } catch(e) {
-        console.log(e)
-    }
-}
-faz()
