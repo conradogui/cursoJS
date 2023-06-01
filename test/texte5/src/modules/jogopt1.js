669eb6d9d23221128f6dd4d62f827c0b541a592b
conradogui <gui14fbce@gmail.com>
2023-06-01 18:06:56 -0300

    inicio do jogo de pedra, papel e tesoura

--- test/texte5/src/modules/jogopt1.js
+++ test/texte5/src/modules/jogopt1.js
@@ -1,7 +1,6 @@
 //capturando evento de click e colocando a imagem na div
 export default document.addEventListener('click', (e) => {
     const elementoClicado = e.target
-
     
 
     const pedrai = document.querySelector('.pedraimg')
@@ -10,7 +9,8 @@ export default document.addEventListener('click', (e) => {
     const divi = document.querySelector('.resultado')
 
     if(elementoClicado === pedrai) {
-        divi.appendChild(elementoClicado)        
+        divi.appendChild(elementoClicado) 
+        comput()     
     }  
     if(elementoClicado === papeli) {
         divi.appendChild(elementoClicado)        
@@ -20,5 +20,14 @@ export default document.addEventListener('click', (e) => {
     } 
     
 })
+
+const rand = (max, min) => Math.floor(Math.random() * (max - min) + min)
+
+function comput () {
+    const objetos = document.querySelector('.objetos')
+    for(let i = 0; i.length; i++) {
+        i = objetos.rand(0, 3)
+    }
+}
 
 
--- test/texte5/src/modules/jogopt2.js
+++ test/texte5/src/modules/jogopt2.js
@@ -3,7 +3,12 @@ import click from './jogopt1'
 const rand = (max, min) => Math.floor(Math.random() * (max - min) + min)
 
 function comput () {
-    
+    const objetos = document.querySelector('.objetos')
+    for(let i = 0; i.length; i++) {
+        i = objetos.rand(0, 3)
+    }
 }
+
+
 
 
