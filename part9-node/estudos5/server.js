c9fe6cc032257d244c662cbbb61c4511ef9fa572
conradogui <gui14fbce@gmail.com>
2023-08-29 20:30:12 -0300

    teste2

--- part9-node/estudos5/server.js
+++ part9-node/estudos5/server.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
 })
 
 app.listen(3000, () => {
-    console.log('Acessar https://localhost:3000')
+    console.log('Acessar http://localhost:3000')
     console.log('Servidor executando na porta 3000')
 })
