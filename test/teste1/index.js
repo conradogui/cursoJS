f74f1583b229fb828850b31c9aaa32c366e9e5a1
conradogui <gui14fbce@gmail.com>
2023-11-23 20:26:05 -0300

    revisando JS

--- test/teste1/index.js
+++ test/teste1/index.js
@@ -1,18 +1,32 @@
-function criaPessoa(nome, sobrenome, p, a) {
-    return {
-        nome,
-        sobrenome,
-        peso: p,
-        altura: a,
-        fala(assunto) {
-            return `${nome} ${sobrenome} fala sobre ${assunto}`
-        },
-        get imc() {
-            const dados = this.peso / (this.altura ** 2)
-            return dados.toFixed(1)           
-        }
-    }
+const form = document.querySelector('.formulario')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const pesoStr = document.querySelector('#peso')
+    const alturaStr = document.querySelector('#altura')
+
+    const peso = Number(pesoStr.value)
+    const altura = Number(alturaStr.value)
+
+
+    const imc = calculaImc(peso, altura)
+    const grauImc = grauImc(imc)
+
+})
+
+function calculaImc(peso, altura) {
+    const resu = peso / altura ** 2
+    return resu.toFixed(2)
 }
-const p1 = criaPessoa('Guilherme', 'Conrado', 65, 1.75)
-console.log(p1.fala('Macacos'))
-console.log(p1.imc)
+
+function grauImc(imc) {
+    const grau = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3',]
+    if(imc < 18.50) return grau[0]
+    if(imc < 24.90 && imc > 18.50) return grau[1]
+    if(imc < 29.90 && imc > 25) return grau[2]
+    if(imc < 34.90 && imc > 30) return grau[3]
+    if(imc < 39.9 && imc > 35) return grau[4]
+    if(imc < 18.50) return grau[5]
+
+}
