feda10911bf0ae2d7480a60696521552d4b8009a
conradogui <gui14fbce@gmail.com>
2023-11-24 17:49:31 -0300

    revisando JS

--- test/teste1/index.js
+++ test/teste1/index.js
@@ -5,13 +5,22 @@ form.addEventListener('submit', e => {
 
     const pesoStr = document.querySelector('#peso')
     const alturaStr = document.querySelector('#altura')
+    const divResu = document.querySelector('#resultado')
 
     const peso = Number(pesoStr.value)
     const altura = Number(alturaStr.value)
 
 
     const imc = calculaImc(peso, altura)
-    const grauImc = grauImc(imc)
+    const grau = grauImc(imc)
+    const resposta = `Seu imc é ${imc} e está com ${grau}`
+    
+    if(!peso || !altura) {
+        divResu.innerHTML = `Formulario invalido`
+    } else {
+        divResu.innerHTML = resposta
+    }
+
 
 })
 
@@ -21,12 +30,11 @@ function calculaImc(peso, altura) {
 }
 
 function grauImc(imc) {
-    const grau = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3',]
+    const grau = ['abaixo do peso', 'peso normal', 'sobrepeso', 'obesidade grau 1', 'obesidade grau 2', 'obesidade grau 3',]
     if(imc < 18.50) return grau[0]
     if(imc < 24.90 && imc > 18.50) return grau[1]
     if(imc < 29.90 && imc > 25) return grau[2]
     if(imc < 34.90 && imc > 30) return grau[3]
     if(imc < 39.9 && imc > 35) return grau[4]
     if(imc < 18.50) return grau[5]
-
 }
